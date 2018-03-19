444162a13a8082852b28e1ed286a12067df6b8fb
WEAVE engineer <weave@rsdtl.space.rl.ac.uk>
2018-03-19 18:56:30 +0000

    Move setup.py to package root

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 import numpy
 
-exts = [Extension("measure", 
-                  ["measure.pyx"],
+exts = [Extension("fibmeasure/measure", 
+                  ["fibmeasure/measure.pyx"],
                   include_dirs=[numpy.get_include()]
                   )]
 
 setup(
+	name = 'fibmeasure',
+	version = '1.0',
+	packages = ['fibmeasure'],
     cmdclass = {'build_ext': build_ext},
-    ext_modules = exts,
+    ext_modules = cythonize(exts),
 )
-
